f5a7930a09006b79164d11b8db9b36700f8f8612
Marlo <95742923+Marlo13@users.noreply.github.com>
2024-01-11 16:42:50 +0100

    build: add prettier ignore jsfile, eslint ignore site doc

--- demo.js
+++ demo.js
@@ -1,26 +1,7 @@
-function maFonction() {
-  var x = 1;
-  if (x > 0) {
-    console.log("x est positif");
-  } else {
-    console.log("x est négatif");
-  }
-  for (var i = 0; i < 5; i++) {
-    console.log("Itération " + i);
-  }
-  var y = x + 2;
-  if (y < 10) {
-    console.log("y est inférieur à 10");
-  } else {
-    console.log("y est supérieur ou égal à 10");
-  }
-  function uneAutreFonction() {
-    console.log("Ceci est une autre fonction");
-  }
-  if (x === 1) {
-    console.log("x est égal à 1");
-  } else {
-    console.log("x n'est pas égal à 1");
-  }
-  console.log("Fin de maFonction");
+function calculerExpression() {
+    let resultat = 2 * 3 + ((3 / 4) % 5);
+    return resultat;
 }
+
+let resultatFinal = calculerExpression();
+console.log("Le résultat est : " + resultatFinal);
